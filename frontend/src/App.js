27d515ac621cdaeccce158d39d2242649f06ba3c
Andrés David Arévalo <75802882+adarevalin@users.noreply.github.com>
2024-07-17 13:06:32 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; // Para los estilos CSS personalizados
 
